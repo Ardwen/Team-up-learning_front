5bfc1d1f89f39a0a3c1d405ea82efeea67710349
Owner <owner@Owners-MacBook-Air.local>
2021-05-06 13:58:23 -0500

    update

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://ec2-3-17-9-85.us-east-2.compute.amazonaws.com:5000/`
+  baseURL: `https://backendteamlearning.herokuapp.com/`
 });
